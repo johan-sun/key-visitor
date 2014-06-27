fa3addddbd67a1a0d317c2a97c1d8aa4064012e1
孙渊汇 <syhkiller@163.com>
2014-06-27 15:45:38 +0800

    v1

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 key-visitor
 ===========
 
-visitor object by key
+in python, we can sort or filter list by object's key
+```
+class Vec:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def sum(self):
+        return self.x + self.y + self.z
+
+l = [Vec(1,2,3),Vec(2,3,4), Vec(4,5,6)]
+```
+
+if we want sort l by x, we can call:`sort(l, key=operator.attrgetter('x'))`,
+but if in c++, we may use a function or lambda like this:
+```
+sort(begin(l), end(l), [](Vec const& l, Vec const& r)){ return l.x < r.x });
+```
+
+key-visitor is a convenience library to adaptor the member variable or variable getter
+to the unary or binary function list `std::less<T>`, then it can be rewrite like
+```
+sort(begin(l), end(l), binary_of<less>(&Vec::x));
+```
+
+sort by sum like:
+```
+sort(begin(l), end(l), binary_of<less>(&Vec::sun));
+```
+
+unique vector by sum like
+```
+unique(begin(l), end(l), binary_of<equal>(&Vec::sum));
+```
+
+if a binary function wrapper by a key,
+the function can called both the raw objeact and the return type,
+like 
+
+```
+binary_of<less>(&Vec::x)(v, 0);//test v.x < 0
+binary_of<less>(&Vec::x)(0, v);//test 0 < v.x
+binary_of<less>(&Vec::x)(v1,v2);//test v1.x < v2.x
+```
---
+++ key_visitor.hpp
@@ -0,0 +1,242 @@
+#include  <utility>
+#include  <memory>
+
+template<typename T>
+struct key{
+    typedef T value_type;
+    typedef T base_type;
+    T& operator()(T& t)const{
+        return t;
+    }
+
+    const T& operator()(const T& t)const{
+        return t;
+    }
+};
+
+template<typename V, typename C>
+struct key<V C::*>{
+    typedef V value_type;
+    typedef C base_type;
+    V C::* member;
+
+    key(V C::* m):member(m){}
+
+    V& operator()(C& c)const{
+        return c.*member;
+    }
+    V const& operator()(C const& c)const{
+        return c.*member;
+    }
+    
+    V& operator()(C* c)const{
+        return c->*member;
+    }
+    V const& operator()(C const* c)const{
+        return c->*member;
+    }
+
+    V& operator()(std::shared_ptr<C> ptr)const{
+        return (*this)(ptr.get());
+    }
+
+    V const& operator()(std::shared_ptr<C const> ptr)const{
+        return (*this)(ptr.get());
+    }
+
+    V& operator()(std::weak_ptr<C> ptr)const{
+        return (*this)(ptr.get());
+    }
+    V const& operator()(std::weak_ptr<C const> ptr)const{
+        return (*this)(ptr.get());
+    }
+
+};
+
+template<typename R, typename C>
+struct key<R(C::*)()>{
+    typedef R value_type;
+    typedef C base_type;
+    R(C::*mem_fun)();
+    key(R (C::*mf)()):mem_fun(mf){}
+
+
+    R operator()(C& c)const{
+        return (c.*mem_fun)();
+    }
+    R operator()(C* c)const{
+        return (c->*mem_fun)();
+    }
+    R operator()(std::shared_ptr<C> ptr)const{
+        return (*this)(*ptr);
+    }
+    R operator()(std::weak_ptr<C> ptr)const{
+        return (*this)(*ptr);
+    }
+};
+
+template<typename R, typename C>
+struct key<R(C::*)()const>{
+    typedef R value_type;
+    R(C::*mem_fun)()const;
+    key(R (C::*mf)()const):mem_fun(mf){}
+    R operator()(C const& c)const{
+        return (c.*mem_fun)();
+    }
+    R operator()(C const* c)const{
+        return (c->*mem_fun)();
+    }
+    R operator()(std::shared_ptr<C const> ptr)const{
+        return (*this)(*ptr);
+    }
+    R operator()(std::shared_ptr<C> ptr)const{
+        return (*this)(*ptr);
+    }
+    R operator()(std::weak_ptr<C const> ptr)const{
+        return (*this)(*ptr);
+    }
+    R operator()(std::weak_ptr<C> ptr)const{
+        return (*this)(*ptr);
+    }
+};
+
+
+template<typename V, typename C>
+key<V C::*> key_of(V C::* m){
+    return key<V C::*>(m);
+}
+
+template<typename R, typename C>
+key<R(C::*)()> key_of(R(C::*m)()){
+    return key<R(C::*)()>(m);
+}
+
+template<typename R, typename C>
+key<R(C::*)()const> key_of(R(C::*m)()const){
+    return key<R(C::*)()const>(m);
+}
+
+template<typename BinaryFun, typename Key>
+struct binary{
+    BinaryFun binary_fun;
+    Key key;
+    binary(Key const& k):key(k){}
+    binary(BinaryFun const& f, Key const& k):binary_fun(f), key(k){}
+    template<typename T>
+    auto operator()(T&& a, T&& b)->decltype(binary_fun(key(std::forward<T>(a)), key(std::forward<T>(b)))){
+        return binary_fun(key(std::forward<T>(a)), key(std::forward<T>(b)));
+    }
+    template<typename T, typename V>
+    auto operator()(T&& a, V& b)->decltype(binary_fun(key(std::forward<T>(a)), std::forward<V>(b))){
+        return binary_fun(key(std::forward<T>(a)), std::forward<V>(b));
+    }
+    template<typename V, typename T>
+    auto operator()(V&& a, T&& b)->decltype(binary_fun(std::forward<V>(a), key(std::forward<T>(b)))){
+        return binary_fun(std::forward<V>(a), key(std::forward<T>(b)));
+    }
+};
+
+template<template<typename E> class TemplateBinaryFun, typename V, typename C>
+binary<TemplateBinaryFun<V>, key<V C::*> > binary_of(V C::* m){
+    key<V C::* > k(m);
+    return binary<TemplateBinaryFun<V>, key<V C::* > >(k);
+}
+
+template<template<typename E> class TemplateBinaryFun, typename R, typename C>
+binary<TemplateBinaryFun<R>, key<R (C::*)() > > binary_of(R (C::* m)()){
+    key<R(C::*)() > k(m);
+    return binary<TemplateBinaryFun<R>, key<R(C::*)() > >(k);
+}
+
+template<template<typename E> class TemplateBinaryFun, typename R, typename C>
+binary<TemplateBinaryFun<R>, key<R (C::*)()const > > binary_of(R (C::* m)()const){
+    key<R(C::*)()const > k(m);
+    return binary<TemplateBinaryFun<R>, key<R(C::*)()const > >(k);
+}
+
+
+template<typename BinaryFun, typename V, typename C>
+binary<BinaryFun, key<V C::*> > binary_of(BinaryFun fun, V C::* m){
+    key<V C::* > k(m);
+    return binary<BinaryFun, key<V C::* > >(fun, k);
+}
+
+template<typename BinaryFun, typename R, typename C>
+binary<BinaryFun, key<R (C::*)() > > binary_of(BinaryFun fun, R (C::* m)()){
+    key<R(C::*)() > k(m);
+    return binary<BinaryFun, key<R(C::*)() > >(fun, k);
+}
+
+template<typename BinaryFun, typename R, typename C>
+binary<BinaryFun, key<R (C::*)()const > > binary_of(BinaryFun fun, R (C::* m)()const){
+    key<R(C::*)()const > k(m);
+    return binary<BinaryFun, key<R(C::*)()const > >(fun, k);
+}
+
+
+template<typename UnaryFun, typename Key>
+struct unary{
+    UnaryFun unary_fun;
+    Key key;
+    unary(Key const& k):key(k){}
+    unary(UnaryFun const& f, Key const& k):unary_fun(f), key(k){}
+    template<typename T>
+    auto operator()(T&& a)->decltype(unary_fun(key(std::forward<T>(a)))){
+        return unary_fun(key(std::forward<T>(a)));
+    }
+};
+
+template<template<typename E> class TemplateUnaryFun, typename V, typename C>
+unary<TemplateUnaryFun<V>, key<V C::*> > unary_of(V C::* m){
+    key<V C::* > k(m);
+    return unary<TemplateUnaryFun<V>, key<V C::* > >(k);
+}
+
+template<template<typename E> class TemplateUnaryFun, typename R, typename C>
+unary<TemplateUnaryFun<R>, key<R (C::*)() > > unary_of(R (C::* m)()){
+    key<R(C::*)() > k(m);
+    return unary<TemplateUnaryFun<R>, key<R(C::*)() > >(k);
+}
+
+template<template<typename E> class TemplateUnaryFun, typename R, typename C>
+unary<TemplateUnaryFun<R>, key<R (C::*)()const > > unary_of(R (C::* m)()const){
+    key<R(C::*)()const > k(m);
+    return unary<TemplateUnaryFun<R>, key<R(C::*)()const > >(k);
+}
+
+
+template<typename UnaryFun, typename V, typename C>
+unary<UnaryFun, key<V C::*> > unary_of(UnaryFun fun, V C::* m){
+    key<V C::* > k(m);
+    return unary<UnaryFun, key<V C::* > >(fun, k);
+}
+
+template<typename UnaryFun, typename R, typename C>
+unary<UnaryFun, key<R (C::*)() > > unary_of(UnaryFun fun, R (C::* m)()){
+    key<R(C::*)() > k(m);
+    return unary<UnaryFun, key<R(C::*)() > >(fun, k);
+}
+
+template<typename UnaryFun, typename R, typename C>
+unary<UnaryFun, key<R (C::*)()const > > unary_of(UnaryFun fun, R (C::* m)()const){
+    key<R(C::*)()const > k(m);
+    return unary<UnaryFun, key<R(C::*)()const > >(fun, k);
+}
+
+
+#include  <algorithm>
+#include  <functional>
+#include  <iostream>
+using namespace std;
+
+struct Vec{
+    int x, y;
+    int sum()const{
+        return x + y;
+    }
+};
+int main(int argc, char *argv[])
+{
+    auto f = binary_of<less>(&Vec::x);
+    return 0;
+}
